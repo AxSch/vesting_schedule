261ce8a076a87e9fe1af3c7739f080c5eeec1b50
AxSch <alex_schuneman@outlook.com>
2025-05-16 19:17:53 +0100

    Update pkg installs

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="vesting_schedule",
-    version="2.0.0",
+    version="2.1.0",
     packages=find_packages(),
     install_requires=[
         "pydantic",
+        "numba",
+        "numpy",
+        "uvloop",
     ],
     entry_points={
         "console_scripts": [
