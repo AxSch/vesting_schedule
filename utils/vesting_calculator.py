cb537147a27590b212e5132ea269a688691ee128
AxSch <alex_schuneman@outlook.com>
2025-05-16 20:06:28 +0100

    Cleanup vesting_calculator.py

--- utils/vesting_calculator.py
+++ utils/vesting_calculator.py
@@ -19,7 +19,7 @@ class DefaultVestingCalculator(IVestingCalculator):
     def calculate_cancelled_shares(self, events: List[Event], target_date: date) -> Decimal:
         quantities = [
             event.quantity
-            for event in sorted(events, key=lambda e: e.event_date)
+            for event in sorted(events, key=lambda event: event.event_date)
             if event.event_date <= target_date
         ]
         return decimal_sum(quantities)
@@ -27,7 +27,7 @@ class DefaultVestingCalculator(IVestingCalculator):
     def calculate_performance_bonus(self, events: List[Event], target_date: date) -> Decimal:
         total_performance_events = [
             event.quantity
-            for event in sorted(events, key=lambda e: e.event_date)
+            for event in sorted(events, key=lambda event: event.event_date)
             if event.event_date <= target_date
         ]
         result = sum(total_performance_events)
